4658c2c38bc0b6b4c7db9bbc69703b876037f5bc
kws970609 <kws970609@users.noreply.github.com>
2020-03-20 10:59:01 +0900

    프로필 사진 누르면 => 사진선택 화면 이동

--- app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/BaseActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/BaseActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import kr.co.tjoeun.a20200320_01_profilesetting.databinding.ActivityMainBinding;
+
 public abstract class BaseActivity extends AppCompatActivity {
+
+
 
     public Context mContext = this;
 
--- app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
@@ -1,21 +1,51 @@
 package kr.co.tjoeun.a20200320_01_profilesetting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import kr.co.tjoeun.a20200320_01_profilesetting.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setContentView(R.layout.activity_main);
         setupEvents();
         setValues();
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("화면재등장","화면 재등장할때마다 실행");
+    }
+
+    @Override
     public void setupEvents() {
+        binding.profilePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+//                갤러리에서 사진 가져오기
+//                안드로이드가 제공하는 화면 => Intent로 이동.
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivity(intent);
+            }
+        });
 
     }
 
